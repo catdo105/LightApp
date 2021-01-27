848c15174ef618a42aeada28de5e348631696aaa
Catherine Do <catdo105@gmail.com>
2021-01-27 17:18:52 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Catherine Do on 1/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
